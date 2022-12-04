4295fabb3672050b632a04bdde252a8ce050c8cc
m-tonon <math.tonon@gmail.com>
2022-12-04 09:03:39 -0300

    adding user signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,28 @@
+const User = require('../models/user.model.js');
+
 function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
 
-function signup(req, res) {
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city 
+  );
 
+  await user.signup() // stores the user into the database
+
+  res.redirect('/login'); 
+  // always redirect after a submit so if the user reload the page it wont try 
+  // to send a request again
 }
 
 function getLogin(req, res) {
-  //...
+  res.render('customer/auth/login');
 }
 
 module.exports = {
