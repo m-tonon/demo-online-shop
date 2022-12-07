bca02528175c901533de966915f3a9b488a879ec
m-tonon <math.tonon@gmail.com>
2022-12-07 07:29:33 -0300

    handling errors in async code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,7 +5,7 @@ function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const user = new User(
     req.body.email,
     req.body.password,
@@ -15,7 +15,12 @@ async function signup(req, res) {
     req.body.city 
   );
 
-  await user.signup() // stores the user into the database
+  try {
+    await user.signup() // stores the user into the database
+  } catch (error) {
+    next(error); // this returns the default error handler, then it handle the 500 page
+    return;
+  }
 
   res.redirect('/login'); 
   // always redirect after a submit so if the user reload the page it wont try 
@@ -26,9 +31,16 @@ function getLogin(req, res) {
   res.render('customer/auth/login');
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password); 
-  const existingUser = await user.getUserWithSameEmail();
+
+  let existingUser; // needed to make the if condition for existingUser avaliable
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   if (!existingUser) {
     res.redirect('/login');
