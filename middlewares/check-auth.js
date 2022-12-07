7253f7764da30bb4d282328e1fb49aaf57f84422
m-tonon <math.tonon@gmail.com>
2022-12-07 06:48:34 -0300

    checking authentication status

---
+++ middlewares/check-auth.js
@@ -0,0 +1,13 @@
+function checkAuthStatus(req, res, next) {
+  const uid = req.session.uid;  // stores the user id of the session
+
+  if (!uid) { // if there isnt any
+    return next();
+  }
+  
+  res.locals.uid = uid; // make available in all the templates
+  res.locals.isAuth = false;
+  next();
+}
+
+module.exports = checkAuthStatus;
--- util/authentication.js
+++ util/authentication.js
@@ -1,4 +1,4 @@
-function createUserSession(req, res, action) { 
+function createUserSession(req, user, action) { 
   req.session.uid = user._id.toString(); // '.session' is avaliable by the express-session package
   req.session.save(action); // the action will be activated once the session is sucessfully saved 
 }
