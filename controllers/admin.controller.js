098038a0c1bc03707509927d0cc9c775b71a3e8c
m-tonon <math.tonon@gmail.com>
2022-12-10 15:27:20 -0300

    adding product model & storing products in the db

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,3 +1,5 @@
+const Product = require('../models/product.model');
+
 function getProducts (req, res) {
   res.render('admin/products/all-products');
 };
@@ -6,9 +8,18 @@ function getNewProduct (req, res) {
   res.render('admin/products/new-product');
 };
 
-function createNewProduct (req, res) {
-  console.log(req.body);
-  console.log(req.file);
+async function createNewProduct (req, res, next) {
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename // the .filename key that showed at console.log the new-product submit
+  });
+
+  try { // may fail, so try / catch
+    await product.save() // this save to the database
+  } catch (error) {
+    next(error);
+    return;
+  }
   
   res.redirect('/admin/products');
 };
---
+++ models/product.model.js
@@ -0,0 +1,28 @@
+const db = require('../data/database');
+
+class Product {
+  constructor(productData) {
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = +productData.price; // '+' forces a conversion to number
+    this.description = productData.description;
+    this.image = productData.image; // the name of the image file
+    this.imagePath = `product-data/images/${productData.image}`;
+    this.imageUrl = `/products/assets/images/${productData.image}`; // request the image from the server
+  }
+
+  async save() {
+    const productData = {
+      title: this.title,
+      summary: this.summary,
+      price: this.price,
+      description: this.description,
+      image: this.image
+    };
+
+    await db.getDb().collection('products').insertOne(productData);
+  } 
+
+}
+
+module.exports = Product;
