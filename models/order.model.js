3fec5b51c4416759edbe4edcab6aa07b6cfd261f
m-tonon <math.tonon@gmail.com>
2022-12-21 06:15:45 -0300

    adding find methods for orders

--- models/order.model.js
+++ models/order.model.js
@@ -1,3 +1,5 @@
+const mongodb = require('mongodb');
+
 const db = require('../data/database');
 
 class Order {
@@ -18,9 +20,60 @@ class Order {
     this.id = orderId;
   }
 
+  static transformOrderDocument (orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
+
+  static transformOrderDocuments (orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  static async findAll() { // find all the orders
+    const orders = await db
+    .getDb()
+    .collection('orders')
+    .find()
+    .sort({_id: -1})
+    .toArray();
+
+    return this.transformOrderDocument(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId); // convert the userId to the mongodb object
+
+    const orders = await db // get the orders from the db 
+      .getDb()
+      .collection('orders')
+      .find({ 'userData._id': uid }) // filter the id from the userData and not the order object itself
+      .sort({ _id: -1 }) // sort the query by id in descending order (the lastest added first) - mongodb sets a timestamp for each object stored
+      .toArray();
+
+    return this.transformOrderDocuments(orders); // then transform the data to the blueprint
+  }
+
+  static async findById(orderId) { // finding by id for adm interface
+    const order = await db
+    .getDb()
+    .collection('orders')
+    .findOne({ _id: new mongodb.ObjectId(orderId)}); // need convert the id to a mongodb object
+
+    return this.transformOrderDocument(order);
+  }
+
   save() {
     if (this.id) { // updating the order
-
+      const orderId = new mongodb.ObjectId(this.id); // convert
+      return db
+      .getDb()
+      .collection('orders')
+      .updateOne({ _id: orderId}, { $set: {status: this.status} }); // the only update we want is the status
     } else { // saving a new one
       const orderDocument = { // the data that will be store into the database
         userData: this.userData, // data from the user
